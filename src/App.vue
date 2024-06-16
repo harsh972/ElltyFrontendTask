61afe90bbb5af8f195fe8fb2aa69d6d2e86118fc
harshtyagi <harsh.tyagi.dev@datahash.com>
2024-06-16 08:15:23 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+     <div class="d-flex justify-content-center">
+      <checkBoxDesign/>
+    </div>
+  </div>
+</template>
+
+<script>
+import checkBoxDesign from './components/checkboxDesign.vue'
+
+export default {
+  name: 'App',
+  components: {
+    checkBoxDesign
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Montserrat;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #1F2128;
+  margin-top: 60px;
+}
+
+</style>
